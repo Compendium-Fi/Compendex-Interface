c21ecdabf7387359aea08ced1a87322acfc9524f
safwen ben fredj <safwen.benfredj@gmail.com>
2023-04-25 01:15:10 +0200

    fixing seo

--- src/components/SeoParams.tsx
+++ src/components/SeoParams.tsx
@@ -12,6 +12,10 @@ const SeoParams = ({ title, description, keywords }: SeoParams) => {
             <title>{title}</title>
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            {/* <meta property="og:image" content="Your page image URL" /> */}
+            <meta property="og:url" content="sol.compendex.xyz" />
         </Head>
     )
 }
