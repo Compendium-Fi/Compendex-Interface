dd592066c6b8275310bdc8ecacd94b3916514e97
safwen ben fredj <safwen.benfredj@gmail.com>
2023-04-25 01:54:36 +0200

    fixing seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,16 @@
-import { Html,Head, Main, NextScript } from 'next/document'
+import SeoParams from '@/components/SeoParams'
+import { Html, Head, Main, NextScript } from 'next/document'
 //import Head from 'next/head'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <SeoParams
+        title="DEX Trading Terminal | Compendex"
+        description="Introducing the next generation of trading technology: an open-source, professional-grade trading terminal for Solana. This platform aggregates protocols and analytics across Solana to help improve market efficiency and equalize opportunities for all."
+        keywords=""
+      />
       <body>
         <Main />
         <NextScript />
