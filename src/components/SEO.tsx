afd98d8a4144c37a64f0fda4ef0cd6ce0c0e2933
safwen ben fredj <safwen.benfredj@gmail.com>
2023-04-25 18:47:12 +0200

    new fixes

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -4,51 +4,51 @@ import Head from "next/head";
 //import { useIsClient } from '~/hooks'
 
 interface SEOProps {
-  name: string;
-  description: string;
+    name: string;
+    description: string;
 }
 
 const SEO = ({ name, description }: SEOProps) => {
-  const isClient = useIsClient();
+    const isClient = useIsClient();
 
-  const windowURL =
-    isClient && window.location.href ? window.location.href : "";
+    const windowURL =
+        isClient && window.location.href ? window.location.href : "";
 
-  return (
-    <Head>
-      <title>{name}</title>
-      <meta name="description" content={description} />
+    return (
+        <Head>
+            <title>{name}</title>
+            <meta name="description" content={description} />
 
-      <meta property="og:title" content={name} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={windowURL} />
-      <meta property="og:site_name" content="DefiLlama" />
-      <meta
-        property="og:description"
-        content="DefiLlama is a DeFi TVL aggregator. It is committed to providing accurate data without ads or sponsored content, as well as transparency."
-      />
-      <meta
-        property="og:image"
-        content={
-          "https://asset.cloudinary.com/doohfu9i4/4d67af902c174c565718927f9e7634ce"
-        }
-      />
+            <meta property="og:title" content={name} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={windowURL} />
+            <meta property="og:site_name" content="Compendex Open Book" />
+            <meta
+                property="og:description"
+                content={description}
+            />
+            <meta
+                property="og:image"
+                content={
+                    "https://res.cloudinary.com/doohfu9i4/image/upload/v1682441159/FtnP_p5XgAcdpMX_nd1emm.jpg"
+                }
+            />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="sol.compendex.xyz" />
-      <meta property="twitter:url" content={windowURL} />
-      <meta name="twitter:title" content={name} />
-      <meta name="twitter:site" content="@Compendexyz" />
-      <meta name="twitter:creator" content="@Compendexyz" />
-      <meta name="twitter:description" content={description} />
-      <meta
-        name="twitter:image"
-        content={
-          "https://asset.cloudinary.com/doohfu9i4/4d67af902c174c565718927f9e7634ce "
-        }
-      />
-    </Head>
-  );
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="twitter:domain" content="sol.compendex.xyz" />
+            <meta property="twitter:url" content={windowURL} />
+            <meta name="twitter:title" content={name} />
+            <meta name="twitter:site" content="@Compendexyz" />
+            <meta name="twitter:creator" content="@Compendexyz" />
+            <meta name="twitter:description" content={description} />
+            <meta
+                name="twitter:image"
+                content={
+                    "https://res.cloudinary.com/doohfu9i4/image/upload/v1682441159/FtnP_p5XgAcdpMX_nd1emm.jpg"
+                }
+            />
+        </Head>
+    );
 };
 
 export default SEO;
